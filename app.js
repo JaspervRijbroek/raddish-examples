4feaeff148807df574c167e6eb6209fc52f6c0aa
Jasper van Rijbroek <jasper@jvar.nl>
2015-02-04 09:09:05 +0100

    Added comments.

--- app.js
+++ app.js
@@ -1,4 +1,13 @@
 var raddish = require('raddish');
 
+// This method accepts a path to a file.
 raddish.setConfig('./config.json');
+
+// To register your application you need to set it into raddish.
 raddish.setApplication('demo', './apps/demo/app.js');
+
+// Check for updates.
+raddish.checkUpdate();
+
+// Start the http(s) server.
+raddish.start();
