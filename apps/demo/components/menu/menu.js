c8ddf39536e346b115b56f4368a8c5b99e630f23
Jasper van Rijbroek <jasper@jvar.nl>
2015-02-04 09:17:37 +0100

    Added some more comments.

--- apps/demo/components/menu/menu.js
+++ apps/demo/components/menu/menu.js
@@ -7,6 +7,16 @@
 function MenuComponent(request, response) {
     ObjectManager.get('com://demo/menu.dispatcher.http')
         .then(function(dispatcher) {
+            /**
+             * After this method most of the magic is happening.
+             * So when calling this component by url (/demo/menu)
+             *
+             * By default the system will look for a database table called menu_menus.
+             * If this table doesn't exists then ofcourse an error is thrown.
+             *
+             * But if you go to this component by the url /home/menu/items
+             * The system will look for the table menu_items.
+             */
 
             // Dispatch the request.
             dispatcher.dispatch(request, response);
