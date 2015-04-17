74125d7c6a4c1e9bdcdaf706a12084a899e1c778
Jasper van Rijbroek <jasper@jvar.nl>
2015-04-17 10:45:06 +0200

    Updated the model to the current version of Raddish.

--- apps/demo/components/todo/model/todo.js
+++ apps/demo/components/todo/model/todo.js
@@ -10,14 +10,14 @@ function TodoModel(config) {
 
 util.inherits(TodoModel, Model);
 
-TodoModel.prototype.buildQueryWhere = function(self, query) {
-    var enabled = self.states.get('enabled').value;
+TodoModel.prototype.buildQueryWhere = function(query) {
+    var enabled = this.states.get('enabled').value;
     
     if(enabled) {
         query.where('tbl.enabled', '=', enabled);
     }
     
-    return self.prototype.buildQueryWhere.call(this, self, query);
+    return Model.prototype.buildQueryWhere.call(this, query);
 };
 
 module.exports = TodoModel;
