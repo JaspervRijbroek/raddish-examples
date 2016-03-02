fd1ee0def1cab57f0e0978b83c6fc0d6452eeb44
Jasper van Rijbroek <jasper@jvar.nl>
2016-03-02 07:09:47 +0100

    Started working on a html layer

--- apps/demo/components/email/email.js
+++
@@ -1,8 +0,0 @@
-function EmailComponent(request, response) {
-    ObjectManager.get('com://demo/email.dispatcher.http')
-        .then(function(dispatcher) {
-            dispatcher.dispatch(request, response);
-        });
-}
-
-module.exports = EmailComponent;
---
+++ apps/teemr/app.js
@@ -0,0 +1,32 @@
+var Application = require('raddish').Application,
+    util = require('util');
+
+function TeemrApplication(config) {
+    config.component = __dirname + '/components';
+    config.
+
+    Application.call(this, config);
+}
+
+util.inherits(TeemrApplication, Application);
+
+TeemrApplication.prototype.runComponent = function(component, request, response) {
+    /**
+     * We will do an override here, because the application now always needs to run com_application first.
+     * After this we will redirect it to the other component.
+     * ALthough there will be some bears on the road.
+     */
+
+    try {
+        var Component = require(this.config.component + '/application/application');
+        new Component(req, res);
+    } catch(error) {
+        if(error instanceof RaddishError) {
+            throw error;
+        }
+
+        throw new RaddishError(404, 'Component "application" not found!');
+    }
+};
+
+module.exports = TeemrApplication;
