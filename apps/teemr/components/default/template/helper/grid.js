54576575d564529b9e65ade99ac5b65be9770025
Jasper van Rijbroek <jasper@jvar.nl>
2016-03-02 18:36:23 +0100

    Added the possibility of helpers and added an example.

---
+++ apps/teemr/components/default/template/helper/grid.js
@@ -0,0 +1,9 @@
+function GridHelper() {
+
+}
+
+GridHelper.prototype.form = function() {
+    return 'return some form here.';
+};
+
+module.exports = GridHelper;
--- apps/teemr/components/default/view/html.js
+++ apps/teemr/components/default/view/html.js
@@ -2,7 +2,7 @@ var View = require('raddish').ViewAbstract,
     util = require('util'),
     fs = require('fs'),
     Application = require('raddish').Application,
-    handlebars = require('handlebars');
+    Handlebars = require('handlebars');
 
 /**
  * This file will be responsibel for getting the hbs file and parsing it.
@@ -38,15 +38,64 @@ ViewHtml.prototype.initialize = function (config) {
  * @returns {String} The rendered HTML.
  */
 ViewHtml.prototype.display = function() {
+    this._registerHelpers();
+
     var identifier = this.data ? this.data.getIdentifier().clone() : this.model.getIdentifier().clone(),
         application = Application.getApplication(identifier.getApplication()),
         path = application.config.component + '/' + identifier.getPackage() + '/view/' + identifier.getName() + '/template.hbs',
-        template = handlebars.compile(fs.readFileSync(path).toString());
+        template = Handlebars.compile(fs.readFileSync(path).toString());
 
     this.data = this.data || {};
+
+    // This line isn't safe! I have to find something for this.
     this.data.component = this.input;
 
     return template(this.data);
+};
+
+ViewHtml.prototype._registerHelpers = function() {
+    /**
+     * There are a few ways we can register helper, we can load them all in. they will be used when they are needed
+     * anyway. Or we can create one method and do it all with that one.
+     *
+     * I am going for the second option. In here we have much more possibilities.
+     */
+    var self = this;
+
+    Handlebars.registerHelper('helper', function() {
+        // we will use the arguments here.
+        var params = Array.prototype.slice.call(arguments),
+            parts = params[0].split('.');
+
+        var identifier = self.getIdentifier().clone();
+        identifier.setPath(['template', 'helper']);
+        identifier.setName(parts[0]);
+
+        console.log('identifier');
+        var Helper = ObjectLoader.require(identifier);
+        if(Helper) {
+            var helper = new Helper();
+            return helper[parts[1]](params.splice(1));
+        } else {
+            return '';
+        }
+    });
+
+    Handlebars.registerHelper('blockHelper', function() {
+
+    });
+
+    Handlebars.registerHelper('modules', function(position) {
+        // Use this to render the modules.
+    });
+
+    Handlebars.registerHelper('count_modules', function(position) {
+        // Count the modules in a certain position.
+    });
+
+    //Handlebars.registerHelper('component', function() {
+    //    return new Handlebars.SafeString(this.input);
+    //});
 };
 
 module.exports = ViewHtml;
